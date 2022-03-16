39cf346d41decb4c5e2b69a68ac90ba1893dfbc7
Radhesh <test@DESKTOP-CVT4GC5>
2022-03-16 10:53:50 +0530

    Initial commit

---
+++ src/main/java/com/tarento/assetManagement/Application.java
@@ -0,0 +1,32 @@
+package com.tarento.assetManagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.tarento.assetManagement.service.HelloWorldService;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application implements CommandLineRunner {
+
+	// Simple example shows how a command line spring application can execute an
+	// injected bean service. Also demonstrates how you can use @Value to inject
+	// command line args ('--name=whatever') or application properties
+
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@Override
+	public void run(String... args) {
+		System.out.println(this.helloWorldService.getHelloMessage());
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
